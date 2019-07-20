fc3130a7144c1224e433c59811c20d5c5bd48f23
Vab <vaibhavgajbhiye14@gmail.com>
2019-07-20 16:03:41 +0530

    Done

--- interaction.js
+++ interaction.js
@@ -9,6 +9,50 @@ var button = document.getElementById("submitplace");
 function servepurpose()
 {
 
+  var flag= true;
+
+  if( nameholder.value=="")
+   {
+     flag= false;
+     var curr= $("#nameplace");
+  
+     curr.popover({
+       trigger: 'manual',
+       placement: 'bottom',
+       content: 'Please Enter a Valid Name' 
+     });
+   
+     curr.popover('show');
+   
+
+   }
+   
+   
+   if(emailholder.value=="")
+   {flag= false;
+   
+    var curr= $("#emailplace");
+  
+  curr.popover({
+    trigger: 'manual',
+    placement: 'bottom',
+    content: 'Please Enter a Valid Email' 
+  });
+
+  curr.popover('show');
+   }
+   
+   if(validateEmail(emailholder.value)==false)
+     flag= false;
+
+   if(flag==false)  
+     return; 
+
+
+   $('#formholder').collapse('hide');
+   $('#sentalert').collapse('show');
+
+
    var data = firebase.database().ref().child("stack");
 
    var member = {
@@ -19,5 +63,48 @@ function servepurpose()
 
    data.push(member);
 
-
 }
+
+
+function validateEmail(email) {
+  var re = /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+  return re.test(email);
+}
+
+
+
+$("#nameplace").keyup(function(){ 
+
+  var curr= $("#nameplace");
+  
+  curr.popover({
+    trigger: 'manual',
+    placement: 'bottom',
+    content: 'Please Enter a Valid Name' 
+  });
+
+  curr.popover('show');
+  
+  if(curr.val()!="")
+     curr.popover('hide');
+
+   });
+
+
+$("#emailplace").keyup(function(){ 
+
+  var curr= $("#emailplace");
+  
+  curr.popover({
+    trigger: 'manual',
+    placement: 'bottom',
+    content: 'Please Enter a Valid Email' 
+  });
+
+  curr.popover('show');
+  
+  if(validateEmail(curr.val()))
+     curr.popover('hide');
+
+   });
+
